a39e7f4eeb07128d9859f2fa7f4ac257d1fc3aad
Pavel <sidorovich.pasha@gmail.com>
2021-12-11 17:22:20 +0300

    install commit

---
+++ ThreadPool.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <queue>
+#include <functional>
+#include <vector>
+
+class ThreadPool
+{
+public:
+	ThreadPool(int _Count);
+	~ThreadPool();
+
+	template<class Fn, class... Args>
+	auto enqueue(Fn&& fn, Args&&... args) ->
+		std::future<decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...))>;
+private:
+	std::mutex mQueueMutex;
+	std::queue<std::function<void(void)>> mTasks;
+	std::condition_variable mCondition;
+	bool mStopPull;
+
+	std::vector<std::thread> mWorkers;
+};
+
+template<class Fn, class... Args>
+auto ThreadPool::enqueue(Fn&& fn, Args&&... args)->
+std::future<decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...))>
+{
+	using ret_t = decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...));
+	std::unique_ptr<int>().release();
+	auto pTask = new std::packaged_task<ret_t()>(
+		std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...));
+
+	std::future<ret_t> res = pTask->get_future();
+
+	std::unique_lock<std::mutex> locker(mQueueMutex);
+	if (mStopPull)
+		throw std::runtime_error("ThreadPull, push task failed, How did you do it?");
+
+	mTasks.emplace([pTask] { (*pTask)(); delete pTask; });
+	locker.unlock();
+	mCondition.notify_one();
+	return res;
+}
